c507f6c163654129601043d7dc1ace1fbee207e4
wzj <1535783491@qq.com>
2019-07-05 16:26:57 +0800

    dubbodemo

---
+++ dubbodemo_provider/src/main/java/com/itheima/service/impl/HelloServiceImpl.java
@@ -0,0 +1,12 @@
+package com.itheima.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.itheima.service.HelloService;
+
+@Service
+public class HelloServiceImpl implements HelloService {
+    @Override
+    public String sayHello(String name) {
+        return "hello"+name;
+    }
+}
